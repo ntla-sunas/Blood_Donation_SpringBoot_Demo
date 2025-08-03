f50a878beb69210e33bf2669ef76cb26f67e5cdb
Nguyễn Lan Anh <nguyen.thi.lan.anh@sun-asterisk.com>
2025-08-03 13:34:36 +0700

    Update exception

--- slide_demo_blood_donation/src/main/java/me/lanying/blooddonation/exception/GlobalExceptionHandler.java
+++ slide_demo_blood_donation/src/main/java/me/lanying/blooddonation/exception/GlobalExceptionHandler.java
@@ -1,4 +1,28 @@
 package me.lanying.blooddonation.exception;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.validation.FieldError;
+import java.util.HashMap;
+import java.util.Map;
+
+@RestControllerAdvice
 public class GlobalExceptionHandler {
+    @ExceptionHandler(ResourceNotFoundException.class)
+    public ResponseEntity<?> handleNotFound(ResourceNotFoundException ex){
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(Map.of("message", ex.getMessage()));
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<?> handleValidation(MethodArgumentNotValidException ex) {
+        Map<String, String> errors = new HashMap<>();
+        for (FieldError err : ex.getBindingResult().getFieldErrors()) {
+            errors.put(err.getField(), err.getDefaultMessage());
+        }
+        return ResponseEntity.badRequest().body(errors);
+    }
+
+
 }
